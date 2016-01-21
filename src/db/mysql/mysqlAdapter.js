9269d9211e32de5b7cdda9fbcef9afa02fdfa655
袁翔 <yemol@xiangdeMac-mini.local>
2016-01-22 01:21:00 +0800

    Update codes using mysql pool get connection.

--- src/db/mysql/mysqlAdapter.js
+++ src/db/mysql/mysqlAdapter.js
@@ -6,25 +6,14 @@ import app from '../../lib/app.js'
 
 
 class Mysql {
-  constructor () {
-    this.connection = null
-  }
-
-  // we have to call this init method for each query to make sure connection is inited correctly.
-  init () {
-    try {
-      this.connection = mysql.createConnection({
-        host: config["mysqlconnect"],
-        user: config["mysqlUserID"],
-        password: config["mysqlPassword"]
-      })
-
-      if (this.connection === null) {
-        throw new Exception("connection cannot be created")
-      }
-    } catch (e) {
-      throw new Exception("Cannot create connection to database! Error is " + e)
-    }
+  // Static constructor, used to create mysql connection pool.
+  static constructor () {
+    this.pool  = mysql.createPool({
+      connectionLimit: config["connectionLimit"],
+      host: config["mysqlconnect"],
+      user: config["mysqlUserID"],
+      password: config["mysqlPassword"]
+    })
   }
 
   // This method is used to get index id of new crated row
@@ -55,52 +44,33 @@ class Mysql {
         myPromise.reject = reject
       })
 
-      this.init()
-      this.connection.connect()
-      app.logger.info("MySQL command: " + command)
-      this.connection.query(command, paramters,
-        (err, result) => {
-          if (err != null) {
-            myPromise.reject(err)
-          } else {
-            myPromise.resolve(result)
-          }
+      this.pool.getConnection( (err, con) => {
+        if (err) {
+          if (con) { con.release() }
+          return myPromise.reject(err)
         }
-      )
-      return myPromise.Promise
+        else {
+          app.logger.info("MySQL command: " + command)
+          con.query(command, paramters,
+            (err, result) => {
+              if (err) {
+                if (con) { con.release() }
+                return myPromise.reject(err)
+              }
+              else {
+                if (con) { con.release() }
+                myPromise.resolve(result)
+              }
+            }
+          )
+          return myPromise.Promise
+        }
+      })
     }
     catch (e) {
       return Promise.reject("Exec insert comment error in database! Error is " + e)
     }
-    finally {
-      if (this.connection !== null) {
-        this.connection.end ()
-        this.connection = null
-      }
-    }
   }
-
-  // connect () {
-  //   try {
-  //     if (this.connection === null) {
-  //       this.connection = mysql.createConnection({
-  //         host: config["mysqlconnect"],
-  //         user: config["mysqlUserID"],
-  //         password: config["mysqlPassword"]
-  //       })
-  //     }
-  //     if (this.connection !== null) {
-  //       this.connection.connect ()
-  //       return true
-  //     } else {
-  //       app.logger.error ("connection cannot be created")
-  //       return false
-  //     }
-  //   } catch (e) {
-  //     app.logger.error ("Cannot create connection to database! Error is " + e, "mysqlAdapter.connect")
-  //     return false
-  //   }
-  // }
 
 }
 
